a1629e92af5c4bdb7ee3bc22d3f0bfdce5eaa527
ashleydavis <ashleydavis75@gmail.com>
2013-06-10 13:42:23 +1000

    Fixed the mouseCapture service to call apply on $rootScope so that the event callbacks take into account changes to the Angular data-model.

--- app.js
+++ app.js
@@ -7,12 +7,35 @@ angular.module('app', ['mouseCapture'])
 //
 // Application controller.
 //
-.controller('AppCtrl', function AppCtrl ($scope) {
+.controller('AppCtrl', function AppCtrl ($scope, mouseCapture) {
 
 	//
 	// Setup the application data-model.
 	//
 	$scope.dataBindingTest = "Hello computer!";
+
+	$scope.mousedown = function (event) {
+
+		mouseCapture.startDrag(event, {
+			dragStarted: function () {
+				console.log("dragStarted");
+
+			},
+
+			dragging: function (deltaX, deltaY) {
+				console.log("dragging: " + deltaX + ", " + deltaY);
+			},
+
+			dragEnded: function () {
+				console.log("dragEnded");
+			},
+
+			clicked: function () {
+				console.log("clicked");
+			},
+		});
+
+	};
 
 })
 
--- mouse_capture_directive.js
+++ mouse_capture_directive.js
@@ -1,20 +1,47 @@
 
 angular.module('mouseCapture', [] )
 
-.directive('mouseCapture', function() {
-  return {
-  	restrict: 'A',
+//
+// Service used to acquire 'mouse capture' then receive dragging events while the mouse is captured.
+//
+.factory('mouseCapture', function ($rootScope) {
 
-  	controller: function($scope, $element, $attrs) {
+	//
+	// Threshold for dragging.
+	// When the mouse moves by at least this amount dragging starts.
+	//
+	var threshold = 5; //todo: make this an attr option.
 
-  		var threshold = 5; //todo: make this an attr option.
+	//
+	// Element that the mouse capture applies to, defaults to 'document' 
+	// unless the 'mouse-capture' directive is used.
+	//
+	var $element = document; 
 
-  		this.startDrag = function (evt, config) {
+	return {
+
+		// 
+		// Register an element to use as the mouse capture element instead of 
+		// the default which is the document.
+		//
+		registerElement: function(element) {
+
+			$element = element;
+		},
+
+		//
+		// Called by users of the service to register a mousedown event and start dragging.
+		// Acquires the 'mouse capture' until the mouseup event.
+		//
+  		startDrag: function (evt, config) {
 
   			var dragging = false;
 			var x = evt.clientX;
 			var y = evt.clientY;
 
+			//
+			// Handler for mousemove events while the mouse is 'captured'.
+			//
 	  		var mouseMove = function (evt) {
 	  			
 				if (!dragging) {
@@ -26,7 +53,7 @@ angular.module('mouseCapture', [] )
 						if (config.dragStarted) {
 							config.dragStarted();
 
-							$scope.$apply();
+							$rootScope.$apply();
 						}
 
 					}
@@ -35,7 +62,7 @@ angular.module('mouseCapture', [] )
 					if (config.dragging) {
 						config.dragging(evt.clientX - x, evt.clientY - y);
 
-						$scope.$apply();
+						$rootScope.$apply();
 					}
 
 					x = evt.clientX;
@@ -43,6 +70,10 @@ angular.module('mouseCapture', [] )
 				}
 	  		};
 
+			//
+			// Handler for mouseup event while the mouse is 'captured'.
+			// Mouseup releases the mouse capture.
+			//
 	  		var mouseUp = function (evt) {
 
 	  			if (dragging) {
@@ -56,7 +87,7 @@ angular.module('mouseCapture', [] )
   					}
 	  			}
 
-				$scope.$apply();
+				$rootScope.$apply();
 
 	  			$element.unbind("mousemove", mouseMove);
 	  			$element.unbind("mouseup", mouseUp);
@@ -65,12 +96,35 @@ angular.module('mouseCapture', [] )
 	  			evt.preventDefault();
 	  		};
 
+	  		// 
+	  		// In response to the mousedown event register handlers for mousemove and mouseup 
+	  		// during 'mouse capture'.
+	  		//
   			$element.mousemove(mouseMove);
   			$element.mouseup(mouseUp);
 
 	  		evt.stopPropagation();
 	  		evt.preventDefault();
-  		};
+  		},
+
+	};
+
+})
+
+//
+// Directive that marks the mouse capture element.
+//
+.directive('mouseCapture', function () {
+  return {
+  	restrict: 'A',
+
+  	controller: function($scope, $element, $attrs, mouseCapture) {
+
+  		// 
+  		// Register the directives element as the mouse capture element.
+  		//
+  		mouseCapture.registerElement($element);
+
   	},
   };
 })
